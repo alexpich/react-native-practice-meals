d86924c18e9f1ab116267311a63f0df682c89f4d
alexpich <pich.alex01@gmail.com>
2020-05-02 01:51:29 -0700

    Move into a separate component and style the boxes

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+} from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+const CategoryGridTile = (props) => {
+  let TouchableCmp = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+  return (
+    <View style={styles.gridItem}>
+      <TouchableCmp style={{ flex: 1 }} onPress={props.onSelect}>
+        <View
+          style={{ ...styles.container, ...{ backgroundColor: props.color } }}
+        >
+          <Text style={styles.title} numberOfLines={2}>
+            {props.title}
+          </Text>
+        </View>
+      </TouchableCmp>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
+  container: {
+    flex: 1,
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 15,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
+    textAlign: "right",
+  },
+});
+
+export default CategoryGridTile;
