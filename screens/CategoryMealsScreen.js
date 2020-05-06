76d1b036d01c021af48bfd8cb95aaa2a3a06ecbf
alexpich <pich.alex01@gmail.com>
2020-05-05 21:11:09 -0700

    Add favorites

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import { CATEGORIES } from "../data/dummy-data";
 import { MEALS } from "../data/dummy-data";
@@ -7,6 +8,8 @@ import MealList from "../components/MealList";
 
 const CategoryMealsScreen = (props) => {
   const catId = props.navigation.getParam("categoryId");
+
+  const availableMeals = useSelector((state) => state.meals.filteredMeals);
 
   const displayedMeals = MEALS.filter(
     (meal) => meal.categoryIds.indexOf(catId) >= 0
